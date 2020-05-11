6ac79abd094a95ad811fd9bcc8509a37ab0c4a9d
vlopatka <vlopatka@overonix.com>
2020-05-11 17:16:06 +0300

    init

---
+++ src/test/kotlin/com/bp/app/AppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bp.app
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
